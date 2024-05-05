cd8f9671a22ac5cb352e1cf4645298b31280107b
Hakan Özoğlu <mhozoglu@yandex.com.tr>
2024-05-06 02:39:21 +0300

    export

---
+++ src/index.ts
@@ -0,0 +1,10 @@
+export { CanvasBG } from "./canvas-bg";
+export { StarField } from "./samples/star-field";
+export { MouseTracker } from "./samples/mouse-tracker";
+export { Constellation } from "./samples/constellation";
+export type {
+  Particle,
+  CanvasSelector,
+  NotNullCanvasSelector,
+  ChildCanvasBG,
+} from "./types";
