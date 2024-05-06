5312f9403c56e162cf2aec4089a09319c2635d21
Hakan Özoğlu <mhozoglu@yandex.com.tr>
2024-05-06 16:43:31 +0300

    change nesting behavior, remove store field, add layers to access more control over other siblings

--- src/canvas-bg.ts
+++ src/canvas-bg.ts
@@ -8,34 +8,52 @@ import { CanvasSelector, ChildCanvasBG, NotNullCanvasSelector } from "./types";
  * The class can be extended to create custom canvas backgrounds.
  * @remarks CanvasBG instances can be chained together to create complex backgrounds.
  */
-export class CanvasBG {
+export class CanvasBG<Config = Record<string, unknown>> {
   protected canvas?: HTMLCanvasElement;
   protected ctx?: CanvasRenderingContext2D;
 
-  /**
-   * Represents the store for the canvas background.
-   * The store is a key-value store that can be used to store and retrieve data.
-   * The store is shared across all instances of the CanvasBG or extended class.
-   *
-   * When overriding the init method in a ChildCanvasBG class, create a new key on this field
-   * if there is shared data that will be used across different instances. For example:
-   * - MouseTracker class stores mouse positions here.
-   * - Base CanvasBG class stores canvas width and height here.
-   * - StarField class stores star positions here (consumed by Constellation).
-   *
-   * Values created here will remain standalone when used independently.
-   * However, when used in a chained or bound context, they will be merged
-   * with the main instance and share the same reference.
-   */
-
-  protected store: Record<string, unknown> = {
-    size: { width: 0, height: 0 },
+  protected _layers: Record<string, ChildCanvasBG> = {
+    __main: this,
   };
 
-  private boundBackgrounds: ChildCanvasBG[] = [];
-  private animationCallbacks = [] as (() => void)[];
+  /**
+   * Gets the layers associated with the current CanvasBG instance.
+   * The layers are stored as key-value pairs, where the key is the layer name
+   * and the value is the associated CanvasBG instance.
+   * the __main key is reserved for the main CanvasBG instance.
+   *
+   * @example
+   * ```typescript
+   * const bg = new CanvasBG(".bg-canvas")
+   * .use(new StarField(null, { velocity: 0.001, points: 150 }))
+   * .use(new MouseTracker(), "mousetracker") // associate with a key
+   * .use(new Constellation()); // associate without a key (default key is the class name)
+   * console.log(bg.layers.__main === bg); // true
+   * ```
+   * layers is always points same address on all siblings
+   * @example
+   * ```typescript
+   * const mainCanvas = new CanvasBG();
+   * const starField = new StarField(mainCanvas);
+   * const mouseTracker = new MouseTracker(mainCanvas);
+   * mainCanvas
+   *   .use(starField, "starfield")
+   *   .use(mouseTracker, "mousetracker");
+   * console.log(mainCanvas.layers === starField.layers); // true
+   *
+   * @returns The layers associated with the current CanvasBG instance.
+   * @remarks Do not modify this property directly; use the `use` method to associate layers.
+   */
+  public get layers() {
+    return this._layers;
+  }
 
-  protected config?: Record<string, unknown>;
+  protected _size = { width: 0, height: 0 };
+  public get size() {
+    return this._size;
+  }
+
+  protected config: Config = {} as Config;
 
   /**
    * @param canvas The canvas element, selector, or ChildCanvasBG.
@@ -56,63 +74,61 @@ export class CanvasBG {
    * @remarks If you are extending the CanvasBG class with a subclass, make sure to call
    * the superclass constructor using `super()` within the constructor of the subclass.
    */
-  constructor(canvas?: CanvasSelector, config?: Record<string, any>) {
+  constructor(canvas?: CanvasSelector, config?: Config) {
+    if (config) this.config = config;
     if (!canvas) return;
-    this.config = config;
     this.bindContext(canvas);
   }
 
-  private bindContext(host: NotNullCanvasSelector) {
+  private bindContext(host: NotNullCanvasSelector, key?: string) {
     if (typeof host === "string") {
       this.canvas = document.querySelector(host) as HTMLCanvasElement;
     } else if (host instanceof HTMLCanvasElement) {
       this.canvas = host;
     } else if (host instanceof CanvasBG) {
+      if (this.layers.__main !== this) {
+        throw new Error("CanvasBG instance can only be associated with one parent");
+      }
       this.canvas = host.canvas as HTMLCanvasElement;
-      Object.assign(host.store, { ...host.store, ...this.store });
-      this.store = host.store;
-      this.store.size = host.store.size;
-      host.boundBackgrounds.push(this);
+      key = key || this.constructor.name.toLowerCase();
+      this._size = host.size;
+      delete this._layers.main;
+      Object.assign(host.layers, {
+        ...host.layers,
+        [key]: this,
+        ...this.layers,
+      });
+      this._layers = host.layers;
     } else {
       throw new Error("Invalid canvas selector");
     }
     this.init();
 
     if (host) {
-      const size = this.store.size as { width: number; height: number };
-      size.width = this.canvas.clientWidth;
-      size.height = this.canvas.clientHeight;
+      this.size.width = this.canvas.clientWidth;
+      this.size.height = this.canvas.clientHeight;
       this.initializeCanvas();
       this.initResizeListener();
     }
     this.ctx = this.canvas.getContext("2d") as CanvasRenderingContext2D;
-    this.addAnimationCallback(this.draw.bind(this));
   }
   private initResizeListener() {
     window.addEventListener("resize", () => {
       if (!this.canvas) return;
-      const size = this.store.size as { width: number; height: number };
-      size.width = this.canvas.clientWidth;
-      size.height = this.canvas.clientHeight;
+      this.size.width = this.canvas.clientWidth;
+      this.size.height = this.canvas.clientHeight;
       this.initializeCanvas();
     });
   }
   private callAnimationCallbacks() {
-    this.animationCallbacks.forEach((callback) => callback());
-    this.boundBackgrounds.forEach((bg) => {
-      bg.callAnimationCallbacks?.();
+    Object.values(this.layers).forEach((layer) => {
+      layer.draw?.();
     });
-  }
-  private addAnimationCallback(callback: () => void) {
-    setTimeout(() => {
-      this.animationCallbacks.push(callback);
-    }, 10);
   }
   private initializeCanvas() {
     if (!this.canvas) throw new Error("Canvas not initialized");
-    const size = this.store.size as { width: number; height: number };
-    this.canvas.width = size.width;
-    this.canvas.height = size.height;
+    this.canvas.width = this.size.width;
+    this.canvas.height = this.size.height;
   }
 
   /**
@@ -150,8 +166,7 @@ export class CanvasBG {
    */
   public animate() {
     if (!this.ctx) throw new Error("Canvas context not initialized");
-    const size = this.store.size as { width: number; height: number };
-    this.ctx.clearRect(0, 0, size.width, size.height);
+    this.ctx.clearRect(0, 0, this.size.width, this.size.height);
     this.callAnimationCallbacks();
     requestAnimationFrame(() => this.animate());
   }
@@ -161,14 +176,14 @@ export class CanvasBG {
    * This method binds the current instance with the provided `canvasbg` instance,
    * allowing the current instance to inherit properties and functionality
    * from the provided instance.
-   * 
-   * (see {@link animate}() method for a complete example) 
+   *
+   * (see {@link animate}() method for a complete example)
    * @param canvasbg The CanvasBG instance to be associated with.
    * @returns The current CanvasBG instance after association.
    * @remarks Don't override this method; it is used internally to bind the context.
    */
-  public use(canvasbg: ChildCanvasBG) {
-    canvasbg.bindContext(this);
+  public use(canvasbg: ChildCanvasBG, key?: string) {
+    canvasbg.bindContext(this, key);
     return this;
   }
 }
