410634e576ec42ebbaea494fa0363d2a087f14ca
Hakan Özoğlu <mhozoglu@yandex.com.tr>
2024-05-06 16:46:44 +0300

    bind dependencies at config, assertion controls

--- src/samples/constellation.ts
+++ src/samples/constellation.ts
@@ -1,6 +1,8 @@
 import { Delaunay } from "d3-delaunay";
 import { CanvasBG } from "../canvas-bg";
-import { Particle } from "../types";
+import { CanvasSelector, ChildCanvasBG, Particle } from "../types";
+import { StarField } from "./star-field";
+import { MouseTracker } from "./mouse-tracker";
 
 /**
  * Represents a constellation displayed on a canvas.
@@ -23,29 +25,81 @@ import { Particle } from "../types";
  *   .use(new MouseTracker())
  *   .use(new Constellation());
  */
-export class Constellation extends CanvasBG {
+export type ConstellationConfig = {
+  mousetracker: ChildCanvasBG | string;
+  starfield: ChildCanvasBG | string;
+};
+export class Constellation extends CanvasBG<ConstellationConfig> {
+  private dots: Particle[] = [];
+  private mousePosition: [number, number] = [0, 0];
+
+  constructor(
+    canvas: CanvasSelector,
+    config: {
+      mousetracker: ChildCanvasBG | string;
+      starfield: ChildCanvasBG | string;
+    }
+  ) {
+    super(canvas, config);
+  }
+  init() {
+    super.init();
+
+    let starfield;
+    if (typeof this.config.starfield === "string") {
+      starfield = this.layers[this.config.starfield];
+    } else {
+      starfield = this.config.starfield;
+    }
+    if ("dots" in starfield) {
+      this.dots = starfield.dots as Particle[];
+    }
+
+    let mousetracker;
+    if (typeof this.config.mousetracker === "string") {
+      mousetracker = this.layers[this.config.mousetracker];
+    } else {
+      mousetracker = this.config.mousetracker;
+    }
+    if ("mousePosition" in mousetracker) {
+      this.mousePosition = mousetracker.mousePosition as [number, number];
+    }
+  }
   private getDistance(point: Delaunay.Point) {
-    const mousePosition = this.store.mousePosition as [number, number];
+    const mousePosition = this.mousePosition as [number, number];
     return Math.sqrt(
       (point[0] - mousePosition[0]) ** 2 + (point[1] - mousePosition[1]) ** 2
     );
   }
   protected draw() {
     if (!this.ctx) throw new Error("Canvas context not initialized");
-    if (!this.store.dots)
+
+    // Check if dots and mouse position are initialized
+    if (!this.dots)
       throw new Error(
         "Dots not initialized at store, use StarField or similar class to initialize it"
       );
-    if (!this.store.mousePosition)
+    if (!this.mousePosition)
       throw new Error(
         "Mouse position not initialized at store, use MouseTracker to initialize it"
       );
 
-    const mousePosition = this.store.mousePosition as [number, number];
-    const dots = this.store.dots as Particle[];
+    //circle around mouse
+    this.ctx.beginPath();
+    this.ctx.fillStyle = "white";
+    this.ctx.arc(
+      this.mousePosition[0],
+      this.mousePosition[1],
+      2,
+      0,
+      Math.PI * 2
+    );
+    this.ctx.fill();
+
+    const dots = this.dots as Particle[];
     if (dots.length < 5) return;
     const XYs = [
-      mousePosition,
+      this.mousePosition,
       ...(dots.map((point) => point.coordinate) as Delaunay.Point[]),
     ];
 
