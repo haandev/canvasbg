ab964723f20a89871b1b6fcb86cf5b9e3fe7b567
Hakan Özoğlu <mhozoglu@yandex.com.tr>
2024-05-13 16:18:15 +0300

    demo

--- demo/src/bg.ts
+++ demo/src/bg.ts
@@ -1,10 +1,15 @@
-import { CanvasBG, StarField, MouseTracker, Constellation } from "@haandev/canvasbg";
+import {
+  CanvasBG,
+  StarField,
+  MouseTracker,
+  Constellation,
+} from "@haandev/canvasbg";
+//} from "../../src";
 
 const bg = new CanvasBG(".bg-canvas")
-  .use(new StarField(null, { velocity: 0.001, points: 150 })) //
   .use(new MouseTracker())
-  .use(new Constellation());
+  .use(new StarField(null, { speed: { x: 0, y: 1, z: 0 }, feed: "top", points: 150 }), { as: "starfield" })
+  .use(new StarField(null, { speed: { x: 0, y: 0.5, z: 0 }, points: 500, alpha: 0.5, feed: "top" }), { as: "sf" })
+  .use(new Constellation(null, { starfield: "starfield", mousetracker: "mousetracker" }));
 
 bg.animate();
-
-console.log(bg);
